f2cd4ce431cc3eeebbef5ec1f00648d0d343cca0
afrologicinsect <danielamahtoday@gmail.com>
2025-03-09 22:56:09 +0100

    iniital commit

---
+++ agent_folder/app.py
@@ -0,0 +1,39 @@
+import yaml
+import os
+from smolagents import GradioUI, CodeAgent, HfApiModel
+
+# Get current directory path
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+from tools.final_answer import FinalAnswerTool as FinalAnswer
+
+
+
+model = HfApiModel(
+model_id='Qwen/Qwen2.5-Coder-32B-Instruct',
+provider=None,
+)
+
+final_answer = FinalAnswer()
+
+
+with open(os.path.join(CURRENT_DIR, "prompts.yaml"), 'r') as stream:
+    prompt_templates = yaml.safe_load(stream)
+
+agent = CodeAgent(
+    model=model,
+    tools=[],
+    managed_agents=[],
+    max_steps=20,
+    verbosity_level=1,
+    grammar=None,
+    planning_interval=None,
+    name=None,
+    description=None,
+    executor_type='local',
+    executor_kwargs={},
+    max_print_outputs_length=None,
+    prompt_templates=prompt_templates
+)
+if __name__ == "__main__":
+    GradioUI(agent).launch()
